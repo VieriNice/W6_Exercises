d7ff703c815a6dd1ff3b7ddf8767e67f5ded37e0
Vieri <vescamilla1@appdev.org>
2024-10-28 14:15:35 -0400

    changed

--- lesson_10_28_func1.py
+++ lesson_10_28_func1.py
@@ -1,13 +1,40 @@
-inv_gar = 26
-inv_recy = 8
-inv_yw = 14
 
-total_bill = "Your total bill managment bill is: {total: .2f}"
-breakdown = "Total includes garbage {0:.2f}, reclycling {1:.2f}, and yard waste {2:.2f}". format(inv_gar, inv_recy, inv_yw)
+sched1 = "Garbage pickup is Wednesday"
+sched2 = "Recycling pickup is Wednesday"
+sched3 = "Yard waste pickup is Friday"
+
+print(f'''Schedule change! This week due to the holiday, 
+      {sched1.replace("Wednesday", "Thursday")}
+    {sched2.replace ("Wednesday ", "Thursday")}
+        {sched3.replace("Firday", "Saturday")}''')
+holiday_sched = sched1.replace("Wednesday", "Thursday")
 
 
-print(total_bill.format(total = inv_gar + inv_recy + inv_yw))
-print(breakdown)
+
+test_range = range(31)
+for x in test_range:
+    print(x)
+
+
+
+
+
+
+
+
+# inv_gar = 26
+# inv_recy = 8
+# inv_yw = 14
+
+# total_bill = "Your total bill managment bill is: {total: .2f}"
+# breakdown = "Total includes garbage {0:.2f}, reclycling {1:.2f}, and yard waste {2:.2f}". format(inv_gar, inv_recy, inv_yw)
+
+
+# print(total_bill.format(total = inv_gar + inv_recy + inv_yw))
+# print(breakdown)
+
+
+
 
 # print(f'''Your total waste management bill is:
 #       ${format(inv_gar + inv_recy + inv_yw, ".2f")}''')
