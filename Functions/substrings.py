90fd5d11f47f856fcae9a3c1400e9522aa16588a
Vieri <vescamilla1@appdev.org>
2024-10-29 10:19:06 -0400

    created substrings and updated lesson2

---
+++ Functions/substrings.py
@@ -0,0 +1,6 @@
+
+full_name = 'Vieri Escamilla'
+print("Full Name: " + str(full_name))
+print(full_name.find(' '))
+print('First Name: ' + str('Vieri'))
+print('Last Name: ' + str('Escamilla'))
---
+++ lesson2_10_28_function.py
@@ -0,0 +1,26 @@
+# import statistics
+# import math
+
+# gar_bill = [1.33, 4, 14.95, 2, 10, 29.98, 3.46]
+# gar_bill.sort()
+
+# print(f'Total garbage bill is: ${math.fsum(gar_bill)}')
+# print(f'Average line item amount: ${stat(f'Median item amount: ${statistics.median(gar_bill)}')
+# print(f'The mode of line items is: ${statistics.median(gar_bill)}')
+# print(f'Bill sorted by amount: {(gar_biistics.mean(gar_bill)}')
+# printll)}')
+
+
+
+# import random
+
+# gar_bill = [1.33, 4, 14.95, 2, 10, 29.98, 3.46]
+# print(random.choices(gar_bill, k = 2))
+
+
+
+import datetime
+
+now = datetime.datetime.now()
+print(now)
+print(now.strftime())
