204fcbf8a5199d4a9cfd6568b1f40f0f66b169ce
Vieri <vescamilla1@appdev.org>
2024-10-30 16:43:26 -0400

    finished random1.py

--- Functions/random1.py
+++ Functions/random1.py
@@ -6,8 +6,16 @@ random.shuffle(color_list)
 print(color_list)
 
 # This list is being used for the .randint function 
-print(random.randint(0,3))
+print(random.randint(0,15))
 
 # This functions gives me random animals from the list set at only 2
 animal_list = ['Dog', 'Cat', 'Turtle', 'Bird']
 print(random.sample(animal_list, k=2))
+
+# This function will give me a random movie from the list
+movie_list = ['Shrek', 'Star Wars', 'Despicable Me']
+print(random.choice(movie_list))
+
+# 
+animal_list = ['Dog', 'Cat', 'Turtle', 'Bird']
+print(random.choices(animal_list, weights = [4, 2, 3, 2], k = 11))
