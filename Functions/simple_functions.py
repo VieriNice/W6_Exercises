e0c361299e20500ad15c6956901b641d4caaaacd
Vieri <vescamilla1@appdev.org>
2024-10-31 19:48:44 -0400

    created simple_functions.py

---
+++ Functions/simple_functions.py
@@ -0,0 +1,21 @@
+
+def favorite_things(name, movie, band):
+    print(f"My name is {name}")
+    print(f"My favorite movie is {movie}")
+    print(f"My favorite band is {band}")
+
+favorite_things("Vieri Escamilla", "Her", "Linkin Park")
+favorite_things('Gisselle Mora', '500 Days of Summer', 'Los Tigres Del Norte')
+
+def why_im_here(opportunity):
+    print(f"Reason to joining Year-Up United: {opportunity}")
+why_im_here("To step foot in the corporate world and build a career out of this.")
+why_im_here("To have a stable career to support the family I want to build")
+
+def fav_place(places):
+    print(f'My favorite places to visit are {places}')
+fav_place("the movies, ramen restaurants, and bars!")
+fav_place("clothing stores, amusement parks, and parties!")
+    
+
+
