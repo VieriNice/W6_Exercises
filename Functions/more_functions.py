f891a2f1b54e2b8b4224e513e5c4ca510e856ace
Vieri <vescamilla1@appdev.org>
2024-11-01 00:31:43 -0400

    created more_functions.py

---
+++ Functions/more_functions.py
@@ -0,0 +1,14 @@
+
+def display_mailing_label(name, address, city, state, zip):
+    print(f'Customer name: {name}')
+    print(f'Customer Address: {address}')
+    print(f'Customer City: {city}')
+    print(f'Customer State: {state} ')
+    print(f'Customer Zip: {zip}')
+
+display_mailing_label('Vieri Escamilla', '123 East Ave', 'Chicago', 'Illinois', '60636')
+
+def add_numbers(*args):
+    
+
+
