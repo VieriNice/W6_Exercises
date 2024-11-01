ed886e107e850d7174056a72dc838413174a6ca3
Vieri <vescamilla1@appdev.org>
2024-11-01 15:42:38 -0400

    updated more_functions.py

--- Functions/more_functions.py
+++ Functions/more_functions.py
@@ -9,6 +9,18 @@ def display_mailing_label(name, address, city, state, zip):
 display_mailing_label('Vieri Escamilla', '123 East Ave', 'Chicago', 'Illinois', '60636')
 
 def add_numbers(*args):
+    return args
+
+result = add_numbers(2, 4, 6, 8)
+
+sum_result = 0
+
+
+for n in result:
+    sum_result += n
     
+
+print(sum_result)
+
 
 
