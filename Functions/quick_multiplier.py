c4ef07ebfabd0b60b0c7b9089331791303a1092f
Vieri <vescamilla1@appdev.org>
2024-11-03 16:33:44 -0500

    created quick_multiplier.py

---
+++ Functions/quick_multiplier.py
@@ -0,0 +1,27 @@
+
+doubler = lambda n: n * 2
+print(doubler(8))
+print(doubler(-4))
+print(doubler('banana'))
+
+tripler = lambda n: n * 3
+print(tripler(8))
+print(tripler(-4))
+print(tripler('banana'))
+
+
+multiplier = lambda x, y: x * y
+
+quadrupler = multiplier(4)
+quintupler = multiplier(5)
+sextrupler = multiplier(6)
+septupler = multiplier(7)
+octupler = multiplier(8)
+nonupler = multiplier(9)
+decupler = multiplier(10)
+
+
+number = 5
+print(f'Quadrupler: {quadrupler(number)}')
+print(f'Quintrupler: {quintupler(number)}')
+
